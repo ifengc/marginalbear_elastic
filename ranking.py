3b49ae53a7512dd11227a9f066db141f9c55fc30
ifengc <timlmj@gmail.com>
2018-01-22 22:36:42 +0800

    revise url scoring

--- ranking.py
+++ ranking.py
@@ -39,12 +39,17 @@ def avg_pmi(query, results, pairs_cnt, total_pairs_cnt, tokenizer):
             comment_scores.append(comment_score)
             tmp_ans[(comment['comment_pos'], comment['comment_unigram'], comment['comment_origin'], result['title_origin'])] = comment_score * result['score']
 
-        max_comment_score = max(comment_scores)
         for (pos, comment_unigram, comment, title), score in tmp_ans.items():
             if comment_unigram in ans:
                 ans[comment_unigram] = (ans[comment_unigram][0] + score, comment, title)
-            elif pos == 'url':
-                ans[comment_unigram] = (max_comment_score * result['score'], comment, title)
+                comment_scores.append(ans[comment_unigram][0])
+            else:
+                ans[comment_unigram] = (score, comment, title)
+
+        max_comment_score = max(comment_scores)
+        for (pos, comment_unigram, comment, title), score in tmp_ans.items():
+            if pos == 'url':
+                ans[comment_unigram] = (max_comment_score, comment, title)
             else:
                 ans[comment_unigram] = (score, comment, title)
 
