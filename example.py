a7406b78f7a23c98800ba62190774fc0628c3f32
ifengc <timlmj@gmail.com>
2018-01-22 21:22:55 +0800

    add multifield query to example

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ import pickle
 from elasticsearch_dsl.connections import connections
 
 from okcom_tokenizer.tokenizers import CCEmojiJieba, UniGram
-from query import post_search
+from query import post_search, post_multifield_query
 from utils import concat_tokens
 from ranking import avg_pmi
 
@@ -29,6 +29,18 @@ def query_unigram(input_sentence, pairs_cnt, total_pairs_cnt):
     return sorted_ans
 
 
+def query_multifield(input_sentence, pairs_cnt, total_pairs_cnt):
+    query_ccjieba = ccjieba.cut(input_sentence.strip())
+    query_unigram = unigram.cut(input_sentence.strip())
+    results = post_multifield_query(client,
+                                    index='post',
+                                    query_ccjieba=concat_tokens(query_ccjieba, pos=False),
+                                    query_unigram=concat_tokens(query_unigram, pos=False),
+                                    top=100)
+    sorted_ans = avg_pmi(query_unigram, results, pairs_cnt, total_pairs_cnt, tokenizer='unigram')
+    return sorted_ans
+
+
 def main(tokenizer):
     t = time.time()
     print('Loading ' + tokenizer + ' pmi pickle')
@@ -43,6 +55,8 @@ def main(tokenizer):
             sorted_ans = query_ccjieba(input_sentence, pairs_cnt, total_pairs_cnt)
         elif tokenizer == 'unigram':
             sorted_ans = query_unigram(input_sentence, pairs_cnt, total_pairs_cnt)
+        elif tokenizer == 'multifield':
+            sorted_ans = query_multifield(input_sentence, pairs_cnt, total_pairs_cnt)
         print(sorted_ans[:20])
 
 
